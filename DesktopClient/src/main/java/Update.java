009771f71860f47be7e5153e44936fe159727eab
Tyler Kelley <Kelltyl2@isu.edu>
2022-03-14 15:56:36 -0600

    login page setup

---
+++ DesktopClient/src/main/java/Update.java
@@ -0,0 +1,16 @@
+
+import lombok.Getter;
+import org.checkerframework.checker.guieffect.qual.UI;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Update {
+
+    @Getter
+    public static final FXController UIController = new FXController();
+
+    public void Update(){
+    }
+}
+
