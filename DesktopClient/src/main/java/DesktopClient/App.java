d0eea912e931a696a78e518d798c569ec7b2e325
Tyler Kelley <Kelltyl2@isu.edu>
2022-03-20 22:12:53 -0600

    some shapes can now be inserted and moved around on the design page

--- DesktopClient/src/main/java/DesktopClient/App.java
+++ DesktopClient/src/main/java/DesktopClient/App.java
@@ -1,12 +1,7 @@
+package DesktopClient;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
-import lombok.Getter;
-
-import java.net.URL;
 
 import java.io.IOException;
 
---
+++ DesktopClient/src/main/java/DesktopClient/Update.java
@@ -0,0 +1,15 @@
+package DesktopClient;
+
+import lombok.Getter;
+
+public class Update {
+
+    @Getter public static final FXController UIController = new FXController();
+
+    /**
+     * Basic constructor to make a new UI controller
+     */
+    public void Update(){
+    }
+}
+
