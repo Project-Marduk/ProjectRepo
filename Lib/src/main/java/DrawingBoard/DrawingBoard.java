3d1533680b7c734446d407866be5a63f4c97f4cc
lindemandavid8 <lindemandavid8@gmail.com>
2022-03-28 14:04:37 -0600

    fixed DrawingBoard so that it correctly returns an SVG object when returnSVGData is called

--- Lib/src/main/java/DrawingBoard/DrawingBoard.java
+++ Lib/src/main/java/DrawingBoard/DrawingBoard.java
@@ -13,6 +13,10 @@ public class DrawingBoard {
     double yMax;
     DrawingObjectFactory drawingObjectFactory = new DrawingObjectFactory();
     static int idIndex = 0;
+    String svgHead = "<svg contentScriptType=\"text/ecmascript\" width=\"600.0px\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" baseProfile=\"full\"\n" +
+            "    zoomAndPan=\"magnify\" contentStyleType=\"text/css\" height=\"300.0px\" preserveAspectRatio=\"xMidYMid meet\" xmlns=\"http://www.w3.org/2000/svg\"\n" +
+            "    version=\"1.0\">";
+    String svgTail;
 
     public DrawingBoard(double xSize, double ySize){
         xMax = xSize;
@@ -26,6 +30,20 @@ public class DrawingBoard {
 
     public void removeObject(String id){
         objects.remove(id);
+    }
+
+    public String returnSVGData(){
+        String svgData = "";
+        for(String key : objects.keySet()){
+            svgData += "\n" + objects.get(key).getSVGData();
+        }
+
+        return "<svg contentScriptType=\"text/ecmascript\" width=\"" + Double.toString(xMax) + "px\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" baseProfile=\"full\"\n" +
+                "    zoomAndPan=\"magnify\" contentStyleType=\"text/css\" height=\"" + Double.toString(yMax) + "px\" preserveAspectRatio=\"xMidYMid meet\" xmlns=\"http://www.w3.org/2000/svg\"\n" +
+                "    version=\"1.0\">"
+                + svgData
+                + "\n"
+                + "</svg>";
     }
 
 
