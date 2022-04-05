af11be7bf350429d6380ceed806fe5536222ead7
Traae <bloxtraa@isu.edu>
2022-04-05 14:06:54 -0600

    Saving more worke

---
+++ Lib/src/main/java/Servers/Resources/HttpRequestHeader.java
@@ -0,0 +1,25 @@
+package Servers.Connections;
+
+
+/**
+ * An enumeration for the values of our Http Request Headers
+ *
+ * These values are arbitrary, but their usage is not.
+ * Using these strings we can avoid mismatches between client and server.
+ *
+ * @author Traae
+ */
+public enum HttpRequestHeader {
+    name("MardukServerCall"),
+    value("application/json");
+
+    private final String s;
+    private HttpRequestHeader(String s) {
+        this.s = s;
+    }
+    public String get(){
+        return this.s;
+    };
+
+
+}
---
+++ Lib/src/main/java/Servers/Users/Folder.java
@@ -0,0 +1,2 @@
+package Servers.Users;public class Folder {
+}
