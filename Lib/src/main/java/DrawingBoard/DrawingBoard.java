a0819277622a4878db992100712bfb4c67a1751c
Traae <bloxtraa@isu.edu>
2022-04-22 19:33:44 -0600

    Saving the World, step 1/5

--- Lib/src/main/java/DrawingBoard/DrawingBoard.java
+++ Lib/src/main/java/DrawingBoard/DrawingBoard.java
@@ -97,4 +97,19 @@ public class DrawingBoard implements Serializable {
         return objects.get(id).getSVGData();
     }
 
+    public String returnSVGData(){
+        String svgData = "";
+        for(String key : objects.keySet()){
+            svgData += "\n" + objects.get(key).getSVGData();
+            svgData += "\n" + objects.get(key).txtToSVG();
+        }
+
+        return "<svg contentScriptType=\"text/ecmascript\" width=\"" + Double.toString(xMax) + "px\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" baseProfile=\"full \"\n" +
+                "    zoomAndPan=\"magnify\" contentStyleType=\"text/css\" height=\"" + Double.toString(yMax) + "px\" preserveAspectRatio=\"xMidYMid meet\" xmlns=\"http://www.w3.org/2000/svg\"\n" +
+                "    version=\"1.0\">"
+                + svgData
+                + "\n"
+                + "</svg>";
+    }
+
 }
--- Lib/src/main/java/DrawingObjects/IFMLModule.java
+++ Lib/src/main/java/DrawingObjects/IFMLModule.java
@@ -32,16 +32,16 @@ public class IFMLModule extends DrawingObject {
                 new double[]{super.inObject.getParams()[1]*.15}, //2nd dim could be font size however there needs to be a font size to dimension conversion
                 "#OOOOOO", //hex code for black
                 super.inObject.getStyle(),
-                super.x, //x axis stays in line with the up left of the larger square
-                super.y - super.inObject.getParams()[1]*.5, new String[]{""}) //move the y axis of the box to halfway down the height of the larger square
+                super.inObject.getXCord(), //x axis stays in line with the up left of the larger square
+                super.inObject.getYCord() - super.inObject.getParams()[1]*.5, new String[]{""}) //move the y axis of the box to halfway down the height of the larger square
         );
 
         String rightBox = squareToSVG(new InputObject("Square",
                 new double[]{super.inObject.getParams()[1]*.15}, //2nd dim could be font size however there needs to be a font size to dimension conversion
                 "#000000", //hex code for black TODO: UPDATE FILL INPUT FOR DEFAULT VALUES
                 super.inObject.getStyle(),
-                super.x + super.inObject.getParams()[0], //move the x axis to the end of the box
-                super.y - super.inObject.getParams()[1], new String[]{""}) //move the y axis of the box to halfway down the height of the larger square
+                super.inObject.getXCord() + super.inObject.getParams()[0], //move the x axis to the end of the box
+                super.inObject.getYCord() - super.inObject.getParams()[1], new String[]{""}) //move the y axis of the box to halfway down the height of the larger square
         );
 
         return squareToSVG(super.getInObject()) + "\n" + leftBox + "\n" + rightBox;
@@ -55,15 +55,15 @@ public class IFMLModule extends DrawingObject {
                 new double[]{super.inObject.getParams()[1]*.15}, //2nd dim could be font size however there needs to be a font size to dimension conversion
                 "#OOOOOO", //hex code for black
                 super.inObject.getStyle(),
-                super.x, //x axis stays in line with the up left of the larger square
-                super.y - super.inObject.getParams()[1]*.5, new String[]{""}) //move the y axis of the box to halfway down the height of the larger square
+                super.inObject.getXCord(), //x axis stays in line with the up left of the larger square
+                super.inObject.getYCord() - super.inObject.getParams()[1]*.5, new String[]{""}) //move the y axis of the box to halfway down the height of the larger square
         ),
         squareToJavaFX(new InputObject("Square",
                 new double[]{super.inObject.getParams()[1]*.15}, //2nd dim could be font size however there needs to be a font size to dimension conversion
                 "#000000", //hex code for black TODO: UPDATE FILL INPUT FOR DEFAULT VALUES
                 super.inObject.getStyle(),
-                super.x + super.inObject.getParams()[0], //move the x axis to the end of the box
-                super.y - super.inObject.getParams()[1], new String[]{""}) //move the y axis of the box to halfway down the height of the larger square
+                super.inObject.getXCord() + super.inObject.getParams()[0], //move the x axis to the end of the box
+                super.inObject.getXCord() - super.inObject.getParams()[1], new String[]{""}) //move the y axis of the box to halfway down the height of the larger square
         ),
         squareToJavaFX(super.getInObject())).getChildren());
     }
