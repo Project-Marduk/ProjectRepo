29c66da098143e52d93565c670dab4dc1c1259d1
lindemandavid8 <lindemandavid8@gmail.com>
2022-04-24 09:28:53 -0600

    created AJDBC classes for users, folders, and a one to many table for users and their folders

--- Lib/src/main/java/ActiveJDBCObjecs/UserAJDBC.java
+++ Lib/src/main/java/ActiveJDBCObjecs/UserAJDBC.java
@@ -1,7 +1,15 @@
 package ActiveJDBCObjecs;
 
+import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.HasMany;
 import org.javalite.activejdbc.annotations.Table;
 
 @Table("users")
-public class UserAJDBC {
+@HasMany(child = UsersFolders.class, foreignKeyName = "user_id")
+public class UserAJDBC extends Model {
+    UserAJDBC(){}
+
+    public Integer getUserId(){
+        return this.getInteger("id");
+    }
 }
--- Lib/src/main/java/ActiveJDBCObjecs/UsersFolders.java
+++ Lib/src/main/java/ActiveJDBCObjecs/UsersFolders.java
@@ -1,9 +1,11 @@
 package ActiveJDBCObjecs;
 
+
 import org.javalite.activejdbc.Model;
 import org.javalite.activejdbc.annotations.Table;
 
-@Table("folders")
-public class FolderAJDBC extends Model {
-    FolderAJDBC(){}
+@Table("users_folders")
+
+public class UsersFolders extends Model {
+    UsersFolders(){}
 }
